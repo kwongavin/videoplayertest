432162e975e50824d087b962b34a4b7eb880c35f
gavin <kwongavin@icloud.com>
2023-03-08 16:34:38 -0800

    Initial Commit

---
+++ (Gavin)VideoPlayerTest/_Gavin_VideoPlayerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Gavin_VideoPlayerTestApp.swift
+//  (Gavin)VideoPlayerTest
+//
+//  Created by Gavin Kwon on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct _Gavin_VideoPlayerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
